60e8ee5dd4258aa47cd767276d77fff1c0c553b9
avachani <apv9448@nyu.edu>
2024-04-14 17:36:56 -0400

    updated docker stuff, should work!

--- machine-learning-client/model.py
+++ machine-learning-client/model.py
@@ -21,26 +21,26 @@ model = load_model(model_path)
 
 #     # After training, save the model
 #     model.save(model_path)
-data_dir = "training_two/"
-data_dir = pathlib.Path(data_dir).with_suffix('')
+# data_dir = "training_two/"
+# data_dir = pathlib.Path(data_dir).with_suffix('')
 
-image_count = len(list(data_dir.glob('*/*.jpg')))
-print(image_count)
+# image_count = len(list(data_dir.glob('*/*.jpg')))
+# print(image_count)
 
 batch_size = 32
 img_height = 180
 img_width = 180
 
-train_ds = tf.keras.utils.image_dataset_from_directory(
-  data_dir,
-  validation_split=0.3,
-  subset="training",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
+# train_ds = tf.keras.utils.image_dataset_from_directory(
+#   data_dir,
+#   validation_split=0.3,
+#   subset="training",
+#   seed=123,
+#   image_size=(img_height, img_width),
+#   batch_size=batch_size)
 
-class_names = train_ds.class_names
-print(class_names)
+class_names = ["Anger", "Happy", "Neutral", "Sad", "Surprise"]
+# print(class_names)
 
 angry_path = "test_photos/angry_guy.jpg"
 
