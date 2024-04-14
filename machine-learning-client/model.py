56a649c7acf1d049f02fbeb101da61f12bfbbc86
tanuj123-cyber <tvs9709@nyu.edu>
2024-04-14 16:46:27 -0400

    testing

--- machine-learning-client/model.py
+++ machine-learning-client/model.py
@@ -13,50 +13,51 @@ import os
 # Path to the saved model
 model_path = './recog.keras'
 
-# Check if the model exists and load it, otherwise train
-# if os.path.exists(model_path):
 model = load_model(model_path)
-# else:
-#     # Place your model training code here
-
-#     # After training, save the model
-#     model.save(model_path)
-data_dir = "training_two/"
-data_dir = pathlib.Path(data_dir).with_suffix('')
-
-image_count = len(list(data_dir.glob('*/*.jpg')))
-print(image_count)
 
 batch_size = 32
 img_height = 180
 img_width = 180
 
-train_ds = tf.keras.utils.image_dataset_from_directory(
-  data_dir,
-  validation_split=0.3,
-  subset="training",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
+class_names = ["Angry", "Happy", "Neutral", "Sad", "Surprise"]
 
-class_names = train_ds.class_names
-# print(class_names)
+# Connect to MongoDB
+# connect to the database
+cxn = pymongo.MongoClient(os.getenv("MONGO_URI"))
+db = cxn[os.getenv("MONGO_DBNAME")]
+app.secret_key = os.environ.get("SECRET_KEY", "default_secret_key")
 
-# angry_path = "test_photos/angry_guy.jpg"
 
-# img = tf.keras.utils.load_img(
-#     angry_path, target_size=(img_height, img_width)
-# )
-# img_array = tf.keras.utils.img_to_array(img)
-# img_array = tf.expand_dims(img_array, 0) # Create a batch
+if user and "assessments" in user:
+        for assessment in user['assessments']:
+            #check if the assessment is an image or text
+            if 'image_data' in assessment:
+                image_b64 = base64.b64encode(assessment['image_data']).decode('utf-8')
+                assessments_list.append({
+                    'type': 'image',
+                    'image_b64': image_b64,
+                    'currentDate': assessment['currentDate']
+                })
 
-# predictions = model.predict(img_array)
-# score = tf.nn.softmax(predictions[0])
+# Fetch an image from MongoDB
+image_data = collection.find_one({'image_id': 'specific_image_id'})
+image_bytes = image_data['image']
 
-# print(
-#     "This image most likely belongs to {} with a {:.2f} percent confidence."
-#     .format(class_names[np.argmax(score)], 100 * np.max(score))
-# )
+# Convert binary data to image
+image = image.open(io.BytesIO(image_bytes))
+image = image.resize((img_height, img_width))  # Resize the image to match model input
+image = image.convert('RGB')  # Ensure image is in RGB format
+
+img_array = tf.keras.utils.img_to_array(image)
+img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
 # ############
 
 # smile_path = "test_photos/test1.jpg"
